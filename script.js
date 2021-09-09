60124325da7433d1c3474731aabe374dd9125191
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-09-09 16:39:54 -0400

    added 4 arrays and a random character generator for each array. started on generatePassword function

--- script.js
+++ script.js
@@ -45,3 +45,37 @@ function length() {
 // console.log(numbers);
 // console.log(specialCharacters);
 
+var specialCharactersArray = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "}", "~"];
+var uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var randomSpecialCharacter = Math.floor(Math.random() * specialCharactersArray.length);
+var randomUppercaseCharacter = Math.floor(Math.random() * uppercaseArray.length);
+var randomLowercaseCharacter = Math.floor(Math.random() * lowercaseArray.length);
+var randomNumber = Math.floor(Math.random() * numbersArray.length);
+// console.log(randomSpecialCharacter);
+// console.log(randomUppercaseCharacter);
+// console.log(randomLowercaseCharacter);
+// console.log(randomNumber);
+
+var password = [];
+
+function generatePassword(a,b,c,d) {
+    if (lowercase && uppercase && numbers && specialCharacters) {
+        password.push(specialCharactersArray[randomSpecialCharacter]);
+        password.push(uppercaseArray[randomUppercaseCharacter]);
+        password.push(lowercaseArray[randomLowercaseCharacter]);
+        password.push(numbersArray[randomNumber]);
+    }
+    console.log(password);
+}
+
+// criteria();
+// length();
+// generatePassword();
+
+// for (let i = 0; i < passwordLength.length - 4; i++) {
+//     const element = array[i];
+    
+// }
