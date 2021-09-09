9e74af07867e696b2d8f14130c178fe8108141c7
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-09-09 16:02:07 -0400

    created function to gather criteria for password

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var lowercase;
+var uppercase;
+var numbers;
+var specialCharacters;
+
+function criteria() {
+    lowercase = confirm("Do you want lowercase letters?");
+    uppercase = confirm("Do you want uppercase letters?");
+    numbers = confirm("Do you want numbers?");
+    specialCharacters = confirm("Do you want special characters?");
+}
+// criteria();
+// console.log(lowercase);
+// console.log(uppercase);
+// console.log(numbers);
+// console.log(specialCharacters);
